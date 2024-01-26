d6fa559446741d539e53f33252971b5cb7e2d3c2
AustinYarborough <141944747+AustinYarborough@users.noreply.github.com>
2024-01-25 19:32:18 -0600

    Update movies.controller.js

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,50 +1,41 @@
 const moviesService = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-async function list(req, res) {
-  const { is_showing } = req.query;
+async function ifMovieExists(req, res, next) {
+  const foundMovie = await moviesService.read(req.params.movieId);
+  if (foundMovie) {
+    res.locals.movie = foundMovie;
+    return next();
+  }
 
-  const data = is_showing
-    ? await (await moviesService.listNowPlaying()).splice(0, 15)
-    : await moviesService.list();
-
-  res.status(200).json({ data: data });
+  return next({
+    status: 404,
+    message: `Movie does not exist for id: ${req.params.movieId}`,
+  });
 }
 
-function read(req, res) {
-  const { movie: data } = res.locals;
+async function list(req, res, next) {
+  const data = await moviesService.list(req.query.is_showing);
   res.json({ data });
 }
 
-function movieExists(req, res, next) {
-  moviesService
-    .read(req.params.movieId)
-    .then((movie) => {
-      if (movie) {
-        res.locals.movie = movie;
-        return next();
-      }
-      next({ status: 404, message: `Movie cannot be found.` });
-    })
-    .catch(next);
+async function read(req, res, next) {
+  res.json({ data: res.locals.movie });
 }
 
-async function getTheaters(req, res) {
-  const data = await moviesService.getTheaters(res.locals.movie.movie_id);
+async function readTheatersByMovieId(req, res, next) {
+  const data = await moviesService.listTheatersByMovieId(Number(req.params.movieId));
   res.json({ data });
 }
 
-async function getReviews(req, res) {
-  const data = await moviesService.getReviews(res.locals.movie.movie_id);
+async function readReviewsByMovieId(req, res, next) {
+  const data = await moviesService.listReviewsByMovieId(Number(req.params.movieId));
   res.json({ data });
 }
 
 module.exports = {
-  list: [asyncErrorBoundary(list)],
-  read: [movieExists, read],
-  getTheaters: [
-    asyncErrorBoundary(movieExists),
-    asyncErrorBoundary(getTheaters),
-  ],
-  getReviews: [movieExists, asyncErrorBoundary(getReviews)],
+  list: asyncErrorBoundary(list),
+  read: [ifMovieExists, read],
+  readTheatersByMovieId: [ifMovieExists, asyncErrorBoundary(readTheatersByMovieId)],
+  readReviewsByMovieId: [ifMovieExists, asyncErrorBoundary(readReviewsByMovieId)],
 };
