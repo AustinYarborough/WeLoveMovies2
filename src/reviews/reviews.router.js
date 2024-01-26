73130ca7bfc1f88713af5ad3c595356328d9d02c
AustinYarborough <141944747+AustinYarborough@users.noreply.github.com>
2024-01-25 19:34:29 -0600

    Update reviews.router.js

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -2,11 +2,6 @@ const router = require("express").Router();
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router
-  .route("/:reviewId([0-9]+)")
-  .put(controller.update)
-  .delete(controller.delete)
-  .all(methodNotAllowed);
-router.route("/").all(methodNotAllowed);
+router.route("/:reviewId").put(controller.update).delete(controller.delete);
 
 module.exports = router;
